6ede1def834894c8b78fd41645cc05a0aea37c9e
Rafael Guilherme <115187383+RG-96W@users.noreply.github.com>
2023-09-28 20:07:23 -0300

    atualizado bug do cadastro

--- server.js
+++ server.js
@@ -1,7 +1,25 @@
+// import app from "./src/app.js";
+// import express from 'express';
+// import cors from 'cors';
+
+// const PORT = 5000;
+
+// app.use(cors());
+
+// app.listen(PORT, () => {
+//   console.log("Servidor online!");
+// });
 import app from "./src/app.js";
+import express from 'express';
+import cors from 'cors';
 
-const PORT = 3000;
+const PORT = 5000;
+const server = express();
 
-app.listen(PORT, () => {
-  console.log("Servidor online!");
+server.use(cors()); // Configuração do CORS aqui
+
+server.use("/", app);
+
+server.listen(PORT, () => {
+  console.log("Servidor online na porta " + PORT);
 });
--- src/controllers/accountController.js
+++ src/controllers/accountController.js
@@ -10,14 +10,17 @@ class accountController {
             res.status(500).json({ message: `${erro.message} - falha na requisição` });
           }
         };
-    static async accountRegister (req, res) {
-        try {
+        static async accountRegister(req, res) {
+          try {
             const newAccount = await account.create(req.body);
-            res.status(201).json({ message: "Cadastrado com sucesso", account: newAccount})
-        }   catch (erro) {
-            res.status(500).json({ message: `${erro.message} - Falha ao cadastrar conta`})
+            res.status(201).json({ message: "Cadastrado com sucesso", account: newAccount });
+          } catch (error) {
+            console.error('Erro ao processar a solicitação POST:', error);
+            // Envie uma resposta de erro
+            res.status(500).send('Erro interno do servidor.');
+          }
         }
-    }
+        
     static async getPasswordByEmail(req, res) {
         try {
           const { email } = req.body;
