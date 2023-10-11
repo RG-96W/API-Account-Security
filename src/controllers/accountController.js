d8ddba001531f67f8dfb2fd59803c3220b292b8f
Rafael Guilherme <115187383+RG-96W@users.noreply.github.com>
2023-10-11 00:05:02 -0300

    Leve correção, retornar nome nos cookies

--- src/controllers/accountController.js
+++ src/controllers/accountController.js
@@ -65,7 +65,7 @@ class accountController {
             // Se a autenticação for bem-sucedida, você pode simplesmente retornar uma resposta de sucesso
             const token = jwt.sign({ login }, SECRET_KEY_SECURE, { expiresIn: '1h' });
 
-            res.status(200).json({auth: true, token })
+            res.status(200).json({auth: true, token, login })
             // res.status(200).json({ message: 'Autenticação bem-sucedida' });
 
           } catch (error) {
